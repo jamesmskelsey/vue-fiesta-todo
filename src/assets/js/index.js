bc759c59f45ce37534fa771c1d92b8998a0fed9f
James Kelsey <jamesmskelsey@gmail.com>
2022-02-06 13:06:13 -0800

    okay so full stack ain't gonna happen

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,33 @@
-console.log("We're in!");
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "We're in!",
+    newTaskText: "",
+    todos: [],
+  },
+  methods: {
+    handleToggle: function (todo) {
+      todo.completed = !todo.completed;
+      this.saveTodos();
+    },
+    handleSubmit: function (e) {
+      e.preventDefault();
+      this.todos.push({
+        id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+        completed: false,
+        task: this.newTaskText,
+      });
+      this.newTaskText = "";
+      this.saveTodos();
+    },
+    saveTodos: function() {
+      window.localStorage.setItem("todos", JSON.stringify(this.todos));
+    }
+  },
+  mounted: function () {
+    loaded_todos = window.localStorage.getItem("todos");
+    if (loaded_todos) {
+      this.todos = JSON.parse(loaded_todos);
+    }
+  },
+});
